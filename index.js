ac2e39603ca4cc2db5b148f68d19310208b121ce
Adam Butler <adam.butler@compono.com>
2023-11-07 21:25:41 +1000

    Update deps and remove dep on pure js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const prettierConfig = require('./.prettierrc');
+
 module.exports = {
   'parser': '@typescript-eslint/parser',
   'extends': [
+    'airbnb-base',
     'compono',
     'prettier',
   ],
@@ -10,6 +13,55 @@ module.exports = {
     'prettier'
   ],
   'rules': {
+    // http://eslint.org/docs/rules/quotes
+    quotes: [
+      2,
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      },
+    ],
+
+    // http://eslint.org/docs/rules/comma-dangle
+    'comma-dangle': 'off',
+
+    // http://eslint.org/docs/rules/strict
+    strict: [0],
+
+    'generator-star-spacing': [
+      'error',
+      {
+        before: true,
+        after: false,
+      },
+    ],
+
+    // http://eslint.org/docs/rules/no-param-reassign
+    // http://eslint.org/docs/rules/no-use-before-define
+    'no-confusing-arrow': ['error', { allowParens: false }],
+    'no-use-before-define': [2, { functions: false }],
+    'no-param-reassign': [2, { props: false }],
+    'no-await-in-loop': 'off',
+    'no-multi-assign': 'off',
+    'no-plusplus': 'off',
+
+    'arrow-parens': 'off',
+    'func-names': 0,
+
+    'prettier/prettier': ['error', prettierConfig],
+    'max-len': [
+      'error',
+      {
+        code: 100,
+        comments: 100,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+
     'no-shadow': 'off',
     'import/no-extraneous-dependencies': 'off',
     'import/prefer-default-export': 'off',
